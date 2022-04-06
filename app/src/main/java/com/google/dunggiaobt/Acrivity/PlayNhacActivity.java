6795c88a1399d806932aa423ff86850e1dec7a5f
Jerrycr2k1 <82642887+Jerrycr2k1@users.noreply.github.com>
2022-04-06 21:24:51 +0700

    Update PlayNhacActivity.java

--- app/src/main/java/com/google/dunggiaobt/Acrivity/PlayNhacActivity.java
+++ app/src/main/java/com/google/dunggiaobt/Acrivity/PlayNhacActivity.java
@@ -1,10 +1,17 @@
 package com.google.dunggiaobt.Acrivity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.dunggiaobt.Model.BaiHat;
@@ -13,11 +20,22 @@ import com.google.dunggiaobt.R;
 import java.util.ArrayList;
 
 public class PlayNhacActivity extends AppCompatActivity {
+    Toolbar toolbarplaynhac;
+    TextView txtTimeSong,txtTotaltimeSong;
+    SeekBar sktime;
+    ImageButton imgplay,imgrepeat,imgnext,imgpre,imgrandom;
+    ViewPager viewPagerplaynhac;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_nhac);
+        init();
+        GetDataFromIntent();
+
+    }
+
+    private void GetDataFromIntent() {
         Intent intent=getIntent();
         if(intent.hasExtra("cakhuc"))
         {
@@ -33,4 +51,28 @@ public class PlayNhacActivity extends AppCompatActivity {
             }
         }
     }
+//Vu
+    private void init (){
+        toolbarplaynhac = findViewById(R.id.toolbarplaynhac);
+        txtTimeSong = findViewById(R.id.textviewtimesong);
+        txtTotaltimeSong = findViewById(R.id.textviewtotaltimesong);
+        sktime = findViewById(R.id.seekbarsong);
+        imgplay = findViewById(R.id.imagebuttonplay);
+        imgnext = findViewById(R.id.imagebuttonnext);
+        imgpre = findViewById(R.id.imagebuttonpre);
+        imgrandom = findViewById(R.id.imagebuttonsuffle);
+        imgrepeat = findViewById(R.id.imagebuttonrepeat);
+        viewPagerplaynhac =findViewById(R.id.viewpagerplaynhac);
+        setSupportActionBar(toolbarplaynhac);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbarplaynhac.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        toolbarplaynhac.setTitleTextColor(Color.WHITE);
+    }
+    //endVU
+
 }
