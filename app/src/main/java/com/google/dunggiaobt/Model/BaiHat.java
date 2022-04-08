a0a212190aa47b0fafd9a1c149ee65caa7c54349
Jerrycr2k1 <82642887+Jerrycr2k1@users.noreply.github.com>
2022-04-08 15:39:53 +0700

    Add files via upload

--- app/src/main/java/com/google/dunggiaobt/Model/BaiHat.java
+++ app/src/main/java/com/google/dunggiaobt/Model/BaiHat.java
@@ -7,7 +7,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 //vu
 public class BaiHat implements Parcelable {
-    @SerializedName("IdBaiHat")
+    @SerializedName("IdBaihat")
     @Expose
     private String idbaihat;
     @SerializedName("Tenbaihat")
