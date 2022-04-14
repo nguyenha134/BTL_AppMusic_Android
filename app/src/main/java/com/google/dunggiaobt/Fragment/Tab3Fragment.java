9f3a0d4f76780ff90a251d353ce45aae1abb4c8b
CONG <kocangi27@gmail.com>
2022-04-14 07:48:07 +0700

    Cong

--- app/src/main/java/com/google/dunggiaobt/Fragment/Tab3Fragment.java
+++ app/src/main/java/com/google/dunggiaobt/Fragment/Tab3Fragment.java
@@ -1,77 +1,76 @@
 package com.google.dunggiaobt.Fragment;
 
-import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
-import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.loader.content.CursorLoader;
 
 import com.google.dunggiaobt.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Tab3Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class Tab3Fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Tab3Fragment() {
-        // Required empty public constructor
-    }
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        Log.e("phamvantruong", "Fragment 3 " );
-    }
-    @Override
-    public void onResume() {
-        super.onResume();
-        Log.e("phamvantruong", "Fragment 3 " );
-    }
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Tab1Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Tab3Fragment newInstance(String param1, String param2) {
-        Tab3Fragment fragment = new Tab3Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    View view;
+    Toolbar toolbar;
+    Button button;
+    ListView listView;
+    ArrayList<String> songArrayList;
+    ArrayAdapter<String> adapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab3, container, false);
+        view = inflater.inflate(R.layout.fragment_ca_nhan, container, false);
+        toolbar = view.findViewById(R.id.toolbarCaNhan);
+        toolbar.setNavigationOnClickListener(view -> getActivity().finish());
+        button = view.findViewById(R.id.buttonRefesh);
+        listView = view.findViewById(R.id.listViewCaNhan);
+        button.setOnClickListener(view1 -> accessMediaStore());
+
+        return view;
+    }
+
+    private void accessMediaStore(){
+        String [] projection = {
+                MediaStore.MediaColumns.DISPLAY_NAME,
+                MediaStore.MediaColumns.DATE_ADDED,
+                MediaStore.MediaColumns.MIME_TYPE
+        };
+
+        CursorLoader cursorLoader = new CursorLoader(getActivity(), MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, projection, null, null, null);
+
+        Cursor cursor = cursorLoader.loadInBackground();
+
+        if (cursor!= null){
+            cursor.moveToFirst();
+
+            songArrayList = new ArrayList<>();
+
+            while (!cursor.isAfterLast()){
+                for (int i = 0; i < cursor.getColumnCount(); i++) {
+                    if (cursor.getString(i).endsWith(".mp3")){
+                        songArrayList.add(cursor.getString(i));
+                    }
+
+                }
+
+                cursor.moveToNext();
+            }
+            adapter = new ArrayAdapter(getActivity(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, songArrayList);
+            listView.setAdapter(adapter);
+            //cursorLoader.cancelLoad();
+            cursor.close();
+        }
+
     }
 }
